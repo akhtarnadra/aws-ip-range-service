5cdd24104dec469d9bc4f169a98d3f5767069a3c
aali <aal@gk-software.com>
2023-09-05 17:53:18 +0200

    Initial commit

---
+++ src/main/java/com/otto/challenge/AwsIpRangeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.otto.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsIpRangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsIpRangeServiceApplication.class, args);
+	}
+
+}
